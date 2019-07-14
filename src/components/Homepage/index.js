a9ac386d98ee0167575dcf784d02d7beab90273e
jonsanders101 <jonsanders101@aol.com>
2019-07-14 17:53:01 +0100

    Add background

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CreateBondForm from '../CreateBondForm';
 
 export default () => (
-  <div>
+  <div className="homepage">
     <h1>Rent Bond</h1>
     <h2>The New Way to Rent</h2>
     <CreateBondForm />
