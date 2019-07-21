0ec93165bb1ff26d030103e5cb079293adcd9c04
Jon Sanders <jonathan.sanders3@tesco.com>
2019-07-21 20:43:04 +0100

    Remove redundant file; make shallow render global

--- spec/tests/CreateBondForm/BasePage.test.js
+++ spec/tests/CreateBondForm/BasePage.test.js
@@ -1,6 +1,5 @@
 import BasePage from '../../../src/components/BasePage';
 import React from 'react';
-import { shallow } from 'enzyme';
 
 test('SHOULD correctly render <BasePage> component', () => {
   const component = shallow(
